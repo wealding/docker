637d8e4ec42e9069990cdb7897e995bdc345584d
@丁小乐 <test@gong.si>
2015-09-29 00:23:54 +0800

    1.09

--- lnmp/run.sh
+++ lnmp/run.sh
@@ -75,14 +75,6 @@ ImportSql()
 }
 
 # Main
-if [ ${REPLICATION_MASTER} == "**False**" ]; then
-    unset REPLICATION_MASTER
-fi
-
-if [ ${REPLICATION_SLAVE} == "**False**" ]; then
-    unset REPLICATION_SLAVE
-fi
-
 # Initialize empty data volume and create MySQL user
 if [[ ! -d $VOLUME_HOME/mysql ]]; then
     echo "=> An empty or uninitialized MySQL volume is detected in $VOLUME_HOME"
@@ -97,43 +89,9 @@ else
     echo "=> Using an existing volume of MySQL"
 fi
 
-# Set MySQL REPLICATION - MASTER
-if [ -n "${REPLICATION_MASTER}" ]; then
-    echo "=> Configuring MySQL replication as master (1/2) ..."
-    if [ ! -f /replication_set.1 ]; then
-        RAND="$(date +%s | rev | cut -c 1-2)$(echo ${RANDOM})"
-        echo "=> Writting configuration file '${CONF_FILE}' with server-id=${RAND}"
-        sed -i "s/^#server-id.*/server-id = ${RAND}/" ${CONF_FILE}
-        sed -i "s/^#log-bin.*/log-bin = mysql-bin/" ${CONF_FILE}
-        touch /replication_set.1
-    else
-        echo "=> MySQL replication master already configured, skip"
-    fi
-fi
-
-# Set MySQL REPLICATION - SLAVE
-if [ -n "${REPLICATION_SLAVE}" ]; then
-    echo "=> Configuring MySQL replication as slave (1/2) ..."
-    if [ -n "${MYSQL_PORT_3306_TCP_ADDR}" ] && [ -n "${MYSQL_PORT_3306_TCP_PORT}" ]; then
-        if [ ! -f /replication_set.1 ]; then
-            RAND="$(date +%s | rev | cut -c 1-2)$(echo ${RANDOM})"
-            echo "=> Writting configuration file '${CONF_FILE}' with server-id=${RAND}"
-            sed -i "s/^#server-id.*/server-id = ${RAND}/" ${CONF_FILE}
-            sed -i "s/^#log-bin.*/log-bin = mysql-bin/" ${CONF_FILE}
-            touch /replication_set.1
-        else
-            echo "=> MySQL replication slave already configured, skip"
-        fi
-    else
-        echo "=> Cannot configure slave, please link it to another MySQL container with alias as 'mysql'"
-        exit 1
-    fi
-fi
-
-
 echo "=> Starting MySQL ..."
 StartMySQL
-tail -F $LOG &
+tail -F ${LOG} &
 
 # Create admin user and pre create database
 if [ -f /var/lib/mysql/.EMPTY_DB ]; then
@@ -141,50 +99,6 @@ if [ -f /var/lib/mysql/.EMPTY_DB ]; then
     CreateMySQLUser
     OnCreateDB
     rm /var/lib/mysql/.EMPTY_DB
-fi
-
-
-# Import Startup SQL
-if [ -n "${STARTUP_SQL}" ]; then
-    if [ ! -f /sql_imported ]; then
-        echo "=> Initializing DB with ${STARTUP_SQL}"
-        ImportSql
-        touch /sql_imported
-    fi
-fi
-
-# Set MySQL REPLICATION - MASTER
-if [ -n "${REPLICATION_MASTER}" ]; then
-    echo "=> Configuring MySQL replication as master (2/2) ..."
-    if [ ! -f /replication_set.2 ]; then
-        echo "=> Creating a log user ${REPLICATION_USER}:${REPLICATION_PASS}"
-        mysql -uroot -e "CREATE USER '${REPLICATION_USER}'@'%' IDENTIFIED BY '${REPLICATION_PASS}'"
-        mysql -uroot -e "GRANT REPLICATION SLAVE ON *.* TO '${REPLICATION_USER}'@'%'"
-        mysql -uroot -e "reset master"
-        echo "=> Done!"
-        touch /replication_set.2
-    else
-        echo "=> MySQL replication master already configured, skip"
-    fi
-fi
-
-# Set MySQL REPLICATION - SLAVE
-if [ -n "${REPLICATION_SLAVE}" ]; then
-    echo "=> Configuring MySQL replication as slave (2/2) ..."
-    if [ -n "${MYSQL_PORT_3306_TCP_ADDR}" ] && [ -n "${MYSQL_PORT_3306_TCP_PORT}" ]; then
-        if [ ! -f /replication_set.2 ]; then
-            echo "=> Setting master connection info on slave"
-            mysql -uroot -e "CHANGE MASTER TO MASTER_HOST='${MYSQL_PORT_3306_TCP_ADDR}',MASTER_USER='${MYSQL_ENV_REPLICATION_USER}',MASTER_PASSWORD='${MYSQL_ENV_REPLICATION_PASS}',MASTER_PORT=${MYSQL_PORT_3306_TCP_PORT}, MASTER_CONNECT_RETRY=30"
-            mysql -uroot -e "start slave"
-            echo "=> Done!"
-            touch /replication_set.2
-        else
-            echo "=> MySQL replication slave already configured, skip"
-        fi
-    else
-        echo "=> Cannot configure slave, please link it to another MySQL container with alias as 'mysql'"
-        exit 1
-    fi
 fi
 
 fg
